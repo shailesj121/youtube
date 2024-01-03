24cf231b4a8bc21b58201f773dc1e9fd457d559a
shaileshj121 <shaileshbisht9876@gmail.com>
2024-01-04 01:52:23 +0530

    express server config and cookie-parser install

--- src/index.js
+++ src/index.js
@@ -5,4 +5,13 @@ dotenv.config({
     path: './env'
 })
 
-connectdb();
+connectdb()
+.then(() => {
+    const port = process.env.PORT
+    app.listen(port || 8000, ()=> {
+        console.log(`server is running at: http://localhost:${port} `)
+    })
+})
+.catch((err) => {
+console.log(`mongodb connection failed ${err}`)
+})

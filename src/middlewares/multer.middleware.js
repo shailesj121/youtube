e4d4e84807816d336c6d646bd2ea2ef34fc62f58
shaileshj121 <shaileshbisht9876@gmail.com>
2024-01-06 19:19:24 +0530

    some small changes

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -5,8 +5,8 @@ const storage = multer.diskStorage({
       cb(null, "./public/temp")
     },
     filename: function (req, file, cb) {
-      cb(null, file.originalName)
+      cb(null, file.originalname)
     }
   })
   
-  export const upload = multer({ storage: storage })
+  export const upload = multer({ storage,})

87ca2e57f02d00d14fd5816bc6f16da04f0055cf
Shailesh bisht <135024896+shailesj121@users.noreply.github.com>
2024-01-24 20:52:37 +0530

    Update user.controller.js

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,5 @@
 import {asyncHandler} from "../utils/asynchandler.js"
 
-const registerUser = asyncHandler(async (req, res) => {
-    res.status(200). json({
-        message: "ok"
-    })
-})
+const registerUser = asyncHandler(async (req, res) => {res.status(200). json({message: "ok"}) })
 
 export {registerUser}
